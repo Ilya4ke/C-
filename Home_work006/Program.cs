e9ca1e68324eba8f50161fd9f205c2e478ff8786
Ilya Pavlov <>
2022-05-29 23:33:30 +0300

    add Home work 006

---
+++ Home_work006/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int numberA = new Random ().Next(10,100); 
+Console.WriteLine(numberA);   
+int numberB = (numberA % 10); 
+int numberC = (numberA / 10); 
+
+int max = numberB;
+
+if (numberC > max)
+    {
+        max = numberC;
+        Console.WriteLine(max);
+    }   
+else
+{
+    Console.WriteLine(max);
+}
