e5ea464b1c0d26375e38a8eb9c98a94e2f0ba075
Ilya Pavlov <>
2022-05-30 00:20:55 +0300

    add Home Work 008

---
+++ Home_work008/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write ("Введите первое число - ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число - ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA % numberB == 0)
+{
+    Console.WriteLine("Первое число кратно второму.");
+}
+else
+{
+    Console.WriteLine(numberA % numberB);
+}
