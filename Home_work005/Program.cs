d8be862aa9407b496dae35ee8ef95422ac725686
Ilya Pavlov <>
2022-05-29 23:04:45 +0300

    add Home work 005

---
+++ Home_work005/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write ("Введите трехзначное число - ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine((numberA / 10) % 10);
