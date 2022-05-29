f3405a3fee35c926aa749e1bc591d6964d50bb81
Ilya Pavlov <>
2022-05-29 23:55:16 +0300

    add Home work 007

---
+++ Home_work007/Program.cs
@@ -0,0 +1,11 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.Write ("Введите трехзначное число - ");
+int numberA = int.Parse(Console.ReadLine());
+
+int numberB = (numberA / 100);
+int numberC = ((numberA / 10) % 10);
+int numberD = (numberA % 10);
+
+Console.Write(numberB);
+Console.Write(numberD);
+
