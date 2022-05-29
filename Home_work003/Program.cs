1bfc922dede7173a87c0a24199a8da79720e9286
Ilya Pavlov <>
2022-05-29 22:20:19 +0300

    add Home work 003

---
+++ Home_work003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите число: ");
+int numberA = int.Parse (Console.ReadLine());
+
+if(numberA % 2 == 0)
+{
+    Console.Write("Заданное число четное.");
+}
+else
+{
+    Console.Write("Заданное число не является четным.");
+}
