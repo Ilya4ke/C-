7fb27e5065ef9571ebb338e4c7fbd66b078d7ef5
Ilya Pavlov <>
2022-05-29 15:31:50 +0300

    add Home work001

---
+++ Home_work001/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write("Введите чесло А:");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите чесло B:");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите чесло C:");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (max < numberB)
+    {
+    max = numberB;
+    }
+if (max < numberC)
+    {
+    max = numberC;
+    } 
+
+Console.Write("Max= ");
+Console.WriteLine(max);
+
