00f989ba3cac7eb0040b571fdb738d421ef2e50d
Ilya Pavlov <>
2022-06-19 10:51:12 +0300

    add Home_work010_18.06

---
+++ Home_work010_18.06/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Write ("Введите число - ");
+int number = int.Parse(Console.ReadLine());
+number = Math.Abs(number);
+
+if (number < 100)
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+  Console.WriteLine(number % 10);
+} 
