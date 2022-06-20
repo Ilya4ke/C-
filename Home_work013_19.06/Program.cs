8094057311e88b482fde63012be5f36925f51c81
Ilya Pavlov <>
2022-06-20 16:21:36 +0300

    add Home_work013_19.06

---
+++ Home_work013_19.06/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27.
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число - ");
+int N = Convert.ToInt32(Console.ReadLine());
+int X = 1;
+
+while(X<=N)
+{
+    int numbers = X*X*X;
+    Console.Write($" {numbers}");
+    X++;
+}
