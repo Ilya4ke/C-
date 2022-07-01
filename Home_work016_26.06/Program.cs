cdf5a819802a28a01f7f3fceb18ce15f36bbd1e6
Ilya Pavlov <>
2022-07-01 21:53:29 +0300

    add Home_work016_26.06_1

--- Home_work016_26.06/Program.cs
+++ Home_work016_26.06/Program.cs
@@ -3,13 +3,13 @@
 int [] massiv = new int [8];
 int [] funkcMass (int [] arr){
     for (int i = 0; i<arr.Length; i++){
-       arr[i] = new Random().Next(0,10); 
+       arr[i] = new Random().Next(0,10);
     }
     return arr;
 }
 int [] res = funkcMass(massiv);
 
 for (int i = 0; i<res.Length; i++){
-    Console.Write(res[i]);
+    Console.Write($"{res[i]}, ");
 }
 
