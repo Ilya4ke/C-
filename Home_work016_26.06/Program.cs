736e0c5b1bab278c398c13920aad7dec64842eb0
Ilya Pavlov <>
2022-07-08 15:26:35 +0300

    add Home_work20_03.07

--- Home_work016_26.06/Program.cs
+++ Home_work016_26.06/Program.cs
@@ -1,15 +1,18 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-int [] massiv = new int [8];
-int [] funkcMass (int [] arr){
-    for (int i = 0; i<arr.Length; i++){
-       arr[i] = new Random().Next(0,10);
+int[] massiv = new int[8];
+int[] funkcMass(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 10);
     }
     return arr;
 }
-int [] res = funkcMass(massiv);
+int[] res = funkcMass(massiv);
 
-for (int i = 0; i<res.Length; i++){
+for (int i = 0; i < res.Length; i++)
+{
     Console.Write($"{res[i]}, ");
 }
 
---
+++ Home_work20_03.07/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+// через функцию
+int Compare(string[] NumbersArray)
+{
+    int sum = 0;
+    for (int i = 0; i < NumbersArray.Length; i++)
+    {
+        if (Convert.ToInt32(NumbersArray[i]) > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+Console.Write("Введите числа - ");
+string numbers = Convert.ToString(Console.ReadLine());
+string[] NumbersArr = numbers.Split(",");
+Console.Write($"{Compare(NumbersArr)} чисел больше 0");
+
+// int sum = 0;
+// // for (int i = 0; i < NumbersArr.Length; i++)
+// // {
+// //     if (Convert.ToInt32(NumbersArr[i]) > 0)
+// //     {
+// //         sum++;
+// //     }
+// // }
+// // Console.Write($"{sum} чисел больше 0");
