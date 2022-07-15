f32d6747b4daed978a54c6f1eec44384d651c66d
Ilya Pavlov <>
2022-07-15 19:13:01 +0300

    add Home_work27_09.07

---
+++ Home_work27_09.07/Program.cs
@@ -0,0 +1,51 @@
+﻿// // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Console.Clear();
+int n = 2; 
+int m = 2; 
+
+int[,] matrix = new int[n, m];
+Console.WriteLine("Первая матрица");
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0, 9);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int n1 = 2; 
+int m1 = 2; 
+Console.WriteLine("Вторая матрица");
+int[,] matrix2 = new int[n1, m1];
+for (int i = 0; i < matrix2.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        matrix2[i, j] = new Random().Next(0, 9);
+        Console.Write(matrix2[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+Console.WriteLine("Произведение матриц");
+int[,] res = new int[n, m1];
+for (int i = 0; i < res.GetLength(0); i++)
+{
+    for (int j = 0; j < res.GetLength(1); j++)
+    {
+        int sum = 0;
+        for(int k = 0; k < res.GetLength(1); k++)
+        {
+            sum = sum + matrix [i,k] * matrix2[k,j];
+        }
+        res[i, j] = sum;
+        Console.Write(res[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
